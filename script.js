cde2e7115b30932fc9ab39ad551b3c00ca67e089
shax752003 <tesimmarak9@gmail.com>
2024-06-03 19:31:32 +0530

    quote slider added
    quote slider added

--- script.js
+++ script.js
@@ -5,4 +5,29 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
 
-  
+// JavaScript code to toggle active class on quotes
+const quotes = document.querySelectorAll('.quote-slider .quote');
+let index = 0;
+
+function showQuote(n) {
+  quotes.forEach((quote, i) => {
+    quote.classList.remove('active');
+    if (i === n) {
+      quote.classList.add('active');
+    }
+  });
+}
+
+function nextQuote() {
+  index = (index + 1) % quotes.length;
+  showQuote(index);
+}
+
+function prevQuote() {
+  index = (index - 1 + quotes.length) % quotes.length;
+  showQuote(index);
+}
+
+// Automatically toggle quotes every 5 seconds
+setInterval(nextQuote, 5000);
+
